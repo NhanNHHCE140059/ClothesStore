798e379a5723f431910aac1c0cd090a25536140d
Hue Nhan <nhannhhce140059@fpt.edu.vn>
2024-06-18 02:33:02 +0700

    feat: register handler

--- src/java/controller/RegisterController.java
+++ src/java/controller/RegisterController.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import service.AccountService;
+import misc.Hash;
 
 /**
  *
@@ -20,6 +22,66 @@ public class RegisterController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        
+        try {
+            String username = req.getParameter("username");
+            String name = req.getParameter("name"); 
+            String email = req.getParameter("email"); 
+            String phoneNumber = req.getParameter("phone");
+            String password = req.getParameter("password");
+            String repassword = req.getParameter("repassword");
+            String address = req.getParameter("address");
+            
+            boolean flag = false;
+            AccountService aService = new AccountService();
+            Hash hash = new Hash();
+            
+            if (aService.exists(username)) {
+                req.setAttribute("usernameErr", username);
+                req.setAttribute("emailVal", email);
+                req.setAttribute("pwdVal", password);
+                flag = true;
+            } 
+            if (!email.contains("@")) {
+                req.setAttribute("emailErr", "Email is invalid, please enter again");
+                req.setAttribute("userVal", username);
+                req.setAttribute("pwdVal", password);
+                flag = true;
+            } 
+            if (!password.equals(repassword)) {
+                req.setAttribute("repwdErr", "Re-password must be matched password");
+                req.setAttribute("emailVal", email);
+                req.setAttribute("userVal", username);
+                flag = true;
+            }
+            if(!phoneNumber.matches("^(0[3|5|7|8|9])+([0-9]{8})\\b$")){
+                req.setAttribute("phoneErr", "Phone is invalid, please enter again");
+                req.setAttribute("userVal", username);
+                req.setAttribute("pwdVal", password);
+                flag = true;
+            }
+            if(!password.matches("^[a-zA-Z0-9]{6,20}$")){
+                req.setAttribute("pwdErr", "Password must have at least 6 characters");
+                req.setAttribute("userVal", username);
+                req.setAttribute("pwdVal", password);
+                flag = true;
+            }
+            
+            if (flag) {
+                req.setAttribute("pwdVal", password);
+                req.setAttribute("emailVal", email);
+                req.setAttribute("userVal", username);
+                req.setAttribute("phoneVal", phoneNumber);
+                req.setAttribute("addressVal", address);
+                req.setAttribute("nameVal", name);
+                req.setAttribute("repwd", repassword);
+                req.getRequestDispatcher("register.jsp").forward(req, res);
+            } else {
+                aService.createAccount(username, name, email, phoneNumber, hash.getMd5(password), address);
+                res.sendRedirect("login");
+            }
+        } catch (IOException e) {
+            System.out.println("Loi register controller");
+            req.getRequestDispatcher("register.jsp").forward(req, res);
+        }
     }
 }
