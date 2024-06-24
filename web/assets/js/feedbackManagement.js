2ac53f0637a155f5e8aba3b0c350cc72bef428a9
Nguyen Khanh Duy <nguyenkhanhduy92000@gmail.com>
2024-06-24 14:51:22 +0700

    Fix search feedback

--- web/assets/js/feedbackManagement.js
+++ web/assets/js/feedbackManagement.js
@@ -20,7 +20,7 @@ document.querySelectorAll('.menu-item').forEach(item => {
 function searchByPhone(param) {
     var phoneSearch = param.value.trim();
     if (phoneSearch === "") {
-
+        location.reload();
         document.getElementById('pagination').style.display = 'flex';
         return;
     }
@@ -41,29 +41,29 @@ function searchByPhone(param) {
         }
     });
 }
-  function setActive(element, pageIndex) {
-            var links = document.querySelectorAll('.page-link');
-            links.forEach(function(link) {
-                link.classList.remove('active');
-            });
-            element.classList.add('active');
-            localStorage.setItem('activePage', pageIndex); // Save active page index to localStorage
-        }
+function setActive(element, pageIndex) {
+    var links = document.querySelectorAll('.page-link');
+    links.forEach(function (link) {
+        link.classList.remove('active');
+    });
+    element.classList.add('active');
+    localStorage.setItem('activePage', pageIndex); // Save active page index to localStorage
+}
 
-        // Set the initial active page link from localStorage
-        document.addEventListener('DOMContentLoaded', function() {
-            var activePage = localStorage.getItem('activePage');
-            if (activePage) {
-                var links = document.querySelectorAll('.page-link');
-                links.forEach(function(link) {
-                    if (link.textContent === activePage) {
-                        link.classList.add('active');
-                    }
-                });
-            } else {
-                var links = document.querySelectorAll('.page-link');
-                if (links.length > 0) {
-                    links[0].classList.add('active'); // Set the first page link as active initially
-                }
+// Set the initial active page link from localStorage
+document.addEventListener('DOMContentLoaded', function () {
+    var activePage = localStorage.getItem('activePage');
+    if (activePage) {
+        var links = document.querySelectorAll('.page-link');
+        links.forEach(function (link) {
+            if (link.textContent === activePage) {
+                link.classList.add('active');
             }
         });
+    } else {
+        var links = document.querySelectorAll('.page-link');
+        if (links.length > 0) {
+            links[0].classList.add('active'); // Set the first page link as active initially
+        }
+    }
+});
