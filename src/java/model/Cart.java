786f0afcb24c443843d9332d3b5d2eb7c661cc61
Hue Nhan <nhannhhce140059@fpt.edu.vn>
2024-06-25 08:18:38 +0700

    fix: remove author

--- src/java/model/Cart.java
+++ src/java/model/Cart.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-/**
- *
- * @author Nguyen Thanh Thien - CE171253
- */
 
 @Getter
 @Setter
