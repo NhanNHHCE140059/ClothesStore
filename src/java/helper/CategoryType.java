fb1ee353c54b13b6ad8b02c6ebcff2d7c6d12d29
Cao Tai <votai12315@gmail.com>
2024-06-29 23:10:20 +0700

    updatehelper

--- src/java/helper/CategoryType.java
+++ src/java/helper/CategoryType.java
@@ -5,5 +5,5 @@ package helper;
  * @author Huenh
  */
 public enum CategoryType {
-    SHORTS_AND_TROUSERS, T_SHIRT, HAT
+   NONE, SHORTS_AND_TROUSERS, T_SHIRT, HAT
 }
--- src/java/helper/ProductSizeType.java
+++ src/java/helper/ProductSizeType.java
@@ -2,5 +2,5 @@
 package helper;
 
 public enum ProductSizeType {
-    S,M,L,XL,XXL,XXXL,XXXXL,XXXXXL
+    NONE,S,M,L,XL,XXL,XXXL,XXXXL,XXXXXL
 }
