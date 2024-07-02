e2fb9e3de8a1987603f3dbcb20de93de07f3b0d8
Nguyen Khanh Duy <nguyenkhanhduy92000@gmail.com>
2024-07-02 19:45:13 +0700

    fix cate subString des update role in comment

--- src/java/controller/ProductManageController.java
+++ src/java/controller/ProductManageController.java
@@ -5,8 +5,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
+import model.Account;
 import model.Product;
 import model.ProductsVariant;
 import service.ProductService;
@@ -21,6 +23,17 @@ public class ProductManageController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        HttpSession session = req.getSession();
+//                
+//        if(session.getAttribute("account")!=null){
+//            resp.sendRedirect(req.getContextPath()+"/login");
+//            return;
+//        }
+//        Account acc = (Account) session.getAttribute("account");
+//        if(acc.getRole()!= helper.Role.Staff || acc.getRole()!= helper.Role.Admin ){
+//               resp.sendRedirect(req.getContextPath()+"/home");
+//            return;
+//        }
         int indexPage;
                 if (req.getParameter("indexPage") != null) {
                     indexPage = Integer.parseInt(req.getParameter("indexPage"));
