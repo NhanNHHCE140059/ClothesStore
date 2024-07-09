22da5bfde3593161575d6fd8a6dbec615d30a289
lltk202 <lailetuankiet202@gmail.com>
2024-07-09 19:18:49 +0700

    delete web.xml mapping

--- src/java/controller/CategoryFilterController.java
+++ src/java/controller/CategoryFilterController.java
@@ -7,6 +7,7 @@ package controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,6 +19,7 @@ import service.ProductService;
  *
  * @author Admin
  */
+@WebServlet(value = "/CategoryFilterController")
 public class CategoryFilterController extends HttpServlet {
 
     /**
